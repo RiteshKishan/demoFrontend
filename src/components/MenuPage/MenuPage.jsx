4b5b6a7f48a39e68ca521d2ee7aeac8dab1dc11e
RiteshKishan <kishanritesh45@gmail.com>
2024-05-11 16:33:56 +0530

    added setTimeout in first data call

--- src/components/MenuPage/MenuPage.jsx
+++ src/components/MenuPage/MenuPage.jsx
@@ -1,50 +1,63 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import AdminLogo from '../../Assests/admin-icon.svg';
 import "../MenuPage/MenuPage.css";
+import axios from "axios";
 import MenuList from "../menuList/menuList";
-const MenuPage = () => {
-  const [menuData, setMenuData] = useState([]);
-  const [subMenu, setSubMenu] = useState([]);
-  const BASE_URL = "https://demobackend-s85p.onrender.com/";
+import AdminLogo from '../../Assests/admin-icon.svg';
 
-  useEffect(() => {
-    const getFoodData = async () => {
+const MenuPage = () => {
+
+    const [menuData, setMenuData] = useState([]);
+    const [subMenu, setSubMenu] = useState([]);
+    const BASE_URL = "https://demobackend-s85p.onrender.com/";
+    // const BASE_URL = "http://localhost:3000/";
+  
+    useEffect(() => {
+      const getFoodData = async (e) => {
+        console.log('function getting called');
+        try {
+          await axios.get(BASE_URL + "getMenu").then((res) => {
+            setMenuData(res.data);
+            setSubMenu(res.data[0].subMenu)
+            // (res.data).map((i)=>{
+            //   setSubMenu(...subMenu, i.subMenu)
+            // })
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      getFoodData();
+      setTimeout(getFoodData, 500)
+    }, []);
+  
+    const getSubMenuData = async (e) => {
       try {
-        const res = await axios.get(BASE_URL + "getMenu");
-        setMenuData(res.data);
-        setSubMenu(res.data[0].subMenu);
+        await axios
+          .get(BASE_URL + "getSubMenu", {
+            params: { itemName: e.target.innerText },
+          })
+          .then((res) => {
+            setSubMenu([]);
+            setSubMenu(res.data);
+          });
       } catch (err) {
-        console.log("Error fetching menu data:", err);
+        console.log(err);
       }
     };
-    getFoodData();
-  }, []);
-
-  const getSubMenuData = async (itemName) => {
-    try {
-      const res = await axios.get(BASE_URL + "getSubMenu", {
-        params: { itemName },
-      });
-      setSubMenu(res.data);
-    } catch (err) {
-      console.log("Error fetching submenu data:", err);
+    const handleAdminLogin = (e) => {
+        window.location.href = '/AdminLogin';
+    
     }
-  };
-
-  const handleAdminLogin = () => {
-    window.location.href = '/AdminLogin';
-  }
 
   return (
     <div>
       <header className="menuHeader">
-        <div className="headerLogo">Our Digital Menu</div>
+        <div className="headerLogo">Our Menu</div>
         <img src={AdminLogo} alt="" className="AdminLogin" onClick={handleAdminLogin} />
       </header>
       <div className="menubutton">
         {menuData.map((data) => (
-          <button key={data.itemName} onClick={() => getSubMenuData(data.itemName)}>
+          <button key={data.itemName} onClick={getSubMenuData}>
             {data.itemName}
           </button>
         ))}
